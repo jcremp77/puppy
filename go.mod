8a57d53be1fa9efb026fb0a3207a74e1ce2aa128
jcremp77 <jcremp77gmail.com>
2024-07-11 05:34:51 -0600

    001

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jcremp77/puppy
+
+go 1.22.5
+
+require github.com/jcremp77/dog v0.0.0-20240711103139-16d15d6bfa71
---
+++ puppy.go
@@ -0,0 +1,21 @@
+package puppy
+
+import (
+	"github.com/jcremp77/dog"
+)
+
+func Bark() string {
+	return "woof!"
+}
+
+func Barks() string {
+	return "woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
+}
